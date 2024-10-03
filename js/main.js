5e4d1bd9ceff90609253c72cf1557779ed7406b5
XanderRaffin <xander.r@hotmail.com>
2024-10-02 21:40:30 -0600

    finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 // Enter your code below.
+let resource = document.querySelector('div.javascript-resources');
+let button = document.querySelector('#show-resources');
+
+button.addEventListener('click', (event)=>{
+    resource.classList.remove('d-none');
+
+})
+
+
+ resource.addEventListener('mouseover', (event)=>{
+    console.log(event.target);
+    event.target.classList.add('fw-bold');
+ })
+
+ resource.addEventListener('mouseout', (event)=>{
+    event.target.classList.remove('fw-bold');
+ })
+
+ resource.addEventListener('click', (event)=>{
+    event.target.classList.add('fst-italic');
+ })
